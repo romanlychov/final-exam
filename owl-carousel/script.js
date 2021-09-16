ee1d4b1b5a8a5e1bd03015944e9c9e9b17e14e86
RomanLychov <romanlychov@gmail.com>
2021-09-17 00:23:20 +0300

    Сверстал страничку контакты под десктоп и под мобилку

--- owl-carousel/script.js
+++ owl-carousel/script.js
@@ -1,17 +1,20 @@
-$('.owl-carousel').owlCarousel({
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
     loop: true,
     items: 1,
     responsiveRefreshRate: 20,
     autoplay: true,
     autoplayHoverPause: true
+  });
 });
 
+
 $(document).ready(function () {
-    $("#humburger-btn").click(function() {
-      $("#menu-mobile").css("display", "block");
-    });
-  
-    $("#menu-mobile-close").click(function() {
-      $("#menu-mobile").css("display", "none");
-    });
+  $("#humburger-btn").click(function () {
+    $("#menu-mobile").css("display", "block");
   });
+
+  $("#menu-mobile-close").click(function () {
+    $("#menu-mobile").css("display", "none");
+  });
+});
